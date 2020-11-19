8fe210b48b322f35d19626c5d58ce5b4af76bf05
mattmeyerink <meyerink@umich.edu>
2020-11-19 15:38:43 -0600

    Adding db setup

--- app/app.py
+++ app/app.py
@@ -1,20 +1,44 @@
 import flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_moment import Moment
 from config import Config
 
-app = flask.Flask(__name__)
-app.config.from_object(Config)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'authentication.login'
+moment = Moment()
 
-from main import main_bp
-app.register_blueprint(main_bp)
+def create_app():
+    app = flask.Flask(__name__)
+    app.config.from_object(Config)
 
-from news import news_bp
-app.register_blueprint(news_bp)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    #login.init_app(app)
+    moment.init_app(app)
 
-from sports import sports_bp
-app.register_blueprint(sports_bp)
+    from main import main_bp
+    app.register_blueprint(main_bp)
 
-from weather import weather_bp
-app.register_blueprint(weather_bp)
+    from news import news_bp
+    app.register_blueprint(news_bp)
 
-from account import account_bp
-app.register_blueprint(account_bp)
+    from sports import sports_bp
+    app.register_blueprint(sports_bp)
+
+    from weather import weather_bp
+    app.register_blueprint(weather_bp)
+
+    from account import account_bp
+    app.register_blueprint(account_bp)
+
+    from authorization import auth_bp
+    app.register_blueprint(auth_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
