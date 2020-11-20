6dcf86c246abdbfa0c866553398580a17fd185e5
mattmeyerink <meyerink@umich.edu>
2020-11-20 14:38:42 -0600

    Restructuring app folder layout and adding user configuration

--- app/app.py
+++ app/app.py
@@ -17,25 +17,25 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
-    #login.init_app(app)
+    login.init_app(app)
     moment.init_app(app)
 
-    from main import main_bp
+    from blueprints.main import main_bp
     app.register_blueprint(main_bp)
 
-    from news import news_bp
+    from blueprints.news import news_bp
     app.register_blueprint(news_bp)
 
-    from sports import sports_bp
+    from blueprints.sports import sports_bp
     app.register_blueprint(sports_bp)
 
-    from weather import weather_bp
+    from blueprints.weather import weather_bp
     app.register_blueprint(weather_bp)
 
-    from account import account_bp
+    from blueprints.account import account_bp
     app.register_blueprint(account_bp)
 
-    from authorization import auth_bp
+    from blueprints.authorization import auth_bp
     app.register_blueprint(auth_bp)
 
     with app.app_context():
---
+++ app/blueprints/authorization/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from flask_login import UserMixin
+from app import login, db
+
+
+class User(UserMixin, db.Model):
+    """Represents a user. Fields will be obtained through google auth."""
+    __tablename__ = "Basecamp-Users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50))
+    name = db.Column(db.String(100))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<User: {self.id} | {self.email}>"
+            
+
+@login.user_loader
+def load_user(id):
+    """Query the db for user object."""
+    return User.query.get(int(id))
